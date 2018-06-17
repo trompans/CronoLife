fa0ae046b3acb301a4f584475044eafb18c1a502
keova <keova@yahoo.com>
2018-06-18 00:06:34 +0200

    v3

--- src/app/actividad.model.ts
+++ src/app/actividad.model.ts
@@ -11,7 +11,16 @@ export class Actividad {
         this.activo = activo;
     }
     static getActividadesActivas() : Actividad[] {
-       let listaActividadesActivas : Actividad[];
+       let listaActividadesActivas : Actividad[] = new Array<Actividad>();
+
+       let actividad : Actividad = new Actividad(1, "alarm", "Dormir", true);
+       listaActividadesActivas.push(actividad);
+       actividad = new Actividad(2, "walk", " caminar", true);
+       listaActividadesActivas.push(actividad);
+       actividad = new Actividad(3, "bookmarks", "leer", true);
+       listaActividadesActivas.push(actividad);
+       actividad= new  Actividad(4,"pizza", "comer", true);
+       listaActividadesActivas.push(actividad);
 
        return listaActividadesActivas;
     }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Actividad } from '../../app/actividad.model';
+import { ActionSheetController } from 'ionic-angular';
+//import { TranslateModule } from '@ngx-translate/core';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'page-list',
@@ -8,10 +11,21 @@ import { Actividad } from '../../app/actividad.model';
 })
 export class ListPage {
   actvidadSelect: Actividad;
+
   //icons: string[];
   listaActividades: Actividad[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  tituloActionSheet : string;
+  litEditar : string;
+  litBorrar : string;
+  litComenzar : string;
+  litCancelar : string;
+  litDesactivar : string;
+
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public translate: TranslateService,
+              public actionSheetCtrl: ActionSheetController) {
     // If we navigated to this page, we will have an item available as a nav param
     this.actvidadSelect = navParams.get('item');
 
@@ -21,6 +35,7 @@ export class ListPage {
 
     this.listaActividades = Actividad.getActividadesActivas();
 
+    this.obtenerTextos();
   }
 
   itemTapped(event, item) {
@@ -28,5 +43,80 @@ export class ListPage {
     this.navCtrl.push(ListPage, {
       item: item
     });
+  }
+
+  obtenerTextos() {
+    this.translate.get('TITULO_ACTIONSHEET').subscribe(
+      value => {
+        // value is our translated string
+        this.tituloActionSheet = value;
+      }
+    );
+    this.translate.get('EDITAR').subscribe(
+      value => {
+        // value is our translated string
+        this.litEditar = value;
+      }
+    );
+    this.translate.get('BORRAR').subscribe(
+      value => {
+        // value is our translated string
+        this.litBorrar = value;
+      }
+    );
+    this.translate.get('DESACTIVAR').subscribe(
+      value => {
+        // value is our translated string
+        this.litDesactivar = value;
+      }
+    );
+    this.translate.get('COMENZAR').subscribe(
+      value => {
+        // value is our translated string
+        this.litComenzar = value;
+      }
+    );
+    this.translate.get('CANCELAR').subscribe(
+      value => {
+        // value is our translated string
+        this.litCancelar = value;
+      }
+    );
+  }
+
+  mostrarAcciones(idActividad : number) {
+
+    const actionSheet = this.actionSheetCtrl.create({
+      buttons: [
+        {
+          text: this.litComenzar,
+          handler: () => {
+            console.log('Comenzar a contar tiempo');
+          }
+        },{
+          text: this.litEditar,
+          handler: () => {
+            console.log('Editar');
+          }
+        },{
+          text: this.litDesactivar,
+          handler: () => {
+            console.log('Desactivar');
+          }
+        },{
+          text: this.litBorrar,
+          handler: () => {
+            console.log('Borrar');
+          }
+        },{
+          text: this.litCancelar,
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    actionSheet.present();
   }
 }
